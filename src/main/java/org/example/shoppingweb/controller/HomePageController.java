ae6898ed6d920248dd5e0cec95ea50c5374285f8
AnhLDHE180218 <anhldhe180218@fpt.edu.vn>
2025-07-16 00:08:03 +0700

    Create HomePageController.java

---
+++ src/main/java/org/example/shoppingweb/controller/HomePageController.java
@@ -0,0 +1,12 @@
+package org.example.shoppingweb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomePageController {
+    @GetMapping("/index")
+    public String showIndex() {
+        return "index";
+    }
+}
