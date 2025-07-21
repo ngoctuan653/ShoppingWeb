9286dd81102e5d7937349a12a89a8c4c559b3662
Phạm Việt Hoàng <phamviethoangvhhy@gmail.com>
2025-07-22 00:49:45 +0700

    Update Orderdetail.java

--- src/main/java/org/example/shoppingweb/entity/Orderdetail.java
+++ src/main/java/org/example/shoppingweb/entity/Orderdetail.java
@@ -40,10 +40,4 @@ public class Orderdetail {
     @Column(name = "unit_price", nullable = false, precision = 18, scale = 2)
     private BigDecimal unitPrice;
 
-
-    @NotNull
-    @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    @JoinColumn(name = "size_id", nullable = false)
-    private Size size;
-
 }
